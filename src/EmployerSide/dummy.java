1d4852e9f5f228a238c08e92d24b45f20b09232d
architkithania <archit.kithania@gmail.com>
2018-11-18 11:20:09 +0800

    Added the skills.txt

--- src/EmployerSide/dummy.java
+++
@@ -1,8 +0,0 @@
-package EmployerSide;
-
-public class dummy {
-    public static void main(String[] args) {
-        String [] list = {"skills", "education", "experience"};
-        SortedCandidates candidates = new SortedCandidates(2, list, "Data_Analyst");
-    }
-}
